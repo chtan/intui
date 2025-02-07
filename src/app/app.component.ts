0c270d1488f2efaf211fa487dd2f4f4cdc4d0d1f
chtan <chtan@suss.edu.sg>
2025-02-07 12:03:54 +0800

    small edit to test webhook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
   constructor(private http: HttpClient) {
     //console.log(environment.apiUrl);
 
-    this.http.get('http://' + environment.apiUrl + '/welcome/echo?x=12455')
+    this.http.get('http://' + environment.apiUrl + '/welcome/echo?x=177')
       .subscribe(data => {
         console.log(data);
       });
