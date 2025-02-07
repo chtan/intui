c8f0dd671bf7c0cd1fbf572c78e90a3640958709
chtan <chtan@suss.edu.sg>
2025-02-07 09:33:36 +0800

    added debug code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,9 +16,11 @@ export class AppComponent {
   constructor(private http: HttpClient) {
     console.log(environment.apiUrl);
 
+    /*
     this.http.get('http://' + environment.apiUrl + '/welcome/echo?x=123')
       .subscribe(data => {
         console.log(data);
       });
+    */
   }
 }
